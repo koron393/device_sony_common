0055b1085affbef2ed2b8e25e4673b2a4ab491e1
Kousuke Nagatsuka <koron393@gmail.com>
2016-10-28 01:38:32 +0900

    sony-common: Scuash audio support for CM
    sony-common: Enable NewSampleRate representation
    * https://github.com/SonyCM/android_device_sony_common/commit/b17ace80a97497ae7398cef1c0c841cb558d55da
    Fixes compress_offload audio
    audio_hw_primary: start_output_stream: enter: stream(0xb623db00)usecase(3: compress-offload-playback) devices(0x2)
    Signed-off-by: David Viteri <davidteri91@gmail.com>
    
    sony-common: Enable QCOM offload audio
    * https://github.com/SonyCM/android_device_sony_common/commit/98a90d255ccc9116581a744ed2c0a51c6c821e71
    Signed-off-by: David Viteri <davidteri91@gmail.com>
    
    * And add some FEATURES

--- cm-common.mk
+++ cm-common.mk
@@ -27,6 +27,34 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Gello
 
+# Audio
+AUDIO_FEATURE_ENABLED_EXTN_POST_PROC := true
+AUDIO_FEATURE_ENABLED_NEW_SAMPLE_RATE := true
+AUDIO_FEATURE_ENABLED_WMA_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_MULTIPLE_TUNNEL := true
+USE_CUSTOM_AUDIO_POLICY := 1
+
+# Audio configs
+PRODUCT_COPY_FILES += \
+    device/sony/common/rootdir/system/vendor/etc/audio_effects_caf.conf:system/vendor/etc/audio_effects.conf \
+    device/sony/common/rootdir/system/vendor/lib/soundfx/libqcbassboost.so:system/vendor/lib/soundfx/libqcbassboost.so \
+    device/sony/common/rootdir/system/vendor/lib/soundfx/libqcreverb.so:system/vendor/lib/soundfx/libqcreverb.so \
+    device/sony/common/rootdir/system/vendor/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so
+
+# Audio Packages
+PRODUCT_PACKAGES += \
+    audiod \
+    libaudio-resampler
+
+# Audio prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.offload.buffer.size.kb=32 \
+    audio.offload.gapless.enabled=true \
+    audio.offload.pcm.16bit.enable=true \
+    audio.offload.pcm.24bit.enable=true \
+    audio.offload.video=true \
+    av.streaming.offload.enable=true
+
 # Camera package
 PRODUCT_PACKAGES += \
     Snap
