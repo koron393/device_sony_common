1612b47227638c38372f645c3a74ba0a0982906b
David <Davidteri91@gmail.com>
2017-02-06 12:07:14 +0100

    Revert "sony-common: cameraHAL: Advertize correct timestamp source"
    This reverts commit 255373136ae19af395eeff0d996f2787ba974643.
    
    sometimes after record 1080p videos and playback the display rest freeze at first frame

--- common.mk
+++ common.mk
@@ -289,8 +289,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.stats.af.paaf=0 \
     persist.camera.feature.cac=0 \
     persist.camera.ois.disable=0 \
-    persist.camera.zsl.mode=1 \
-    persist.camera.time.monotonic=0
+    persist.camera.zsl.mode=1
 
 ifneq ($(filter shinano rhine, $(SOMC_PLATFORM)),)
 PRODUCT_PROPERTY_OVERRIDES += \
